868f54ddc2f945189fc3e4f0e733288792089929
uerbzr <nigel.sibbert@gmail.com>
2023-04-26 15:51:29 +0100

    Revert "done"
    This reverts commit 6c73bc6da7adcb84caaff6d301bb6ee42c15c5aa.

--- src/csharp-fundamentals-primitive-types.Main/Core.cs
+++ src/csharp-fundamentals-primitive-types.Main/Core.cs
@@ -12,39 +12,39 @@ namespace csharp_fundamentals_primitive_types.Main
 
 
         // 1. Change the value of the member below to be the result of adding numOne and numTwo together
-        public int numOnePlusTwo = numOne + numTwo;
+        public int numOnePlusTwo = 0;
 
 
         // 2. Change the value of the member below to be the result of multiplying numThree by numTwo
-        public int numThreeTimesNumTwo = numThree * numTwo;
+        public int numThreeTimesNumTwo = 0;
 
 
         // 3. Change the value of the member below to be the result of dividing numThree by numOne
-        public int numThreeDividedByNumOne = numThree / numOne;
+        public int numThreeDividedByNumOne = 0;
 
 
         // 4. Change the value of the member below to be the result of subtracting numOne from numThree
-        public int numThreeMinusNumOne = numThree - numOne;
+        public int numThreeMinusNumOne = 0;
 
 
         // 5. Change the value of the member below to be the sum of numOne, numTwo and numThree
-        public int sum = numOne + numTwo + numThree;
+        public int sum = 0;
 
 
         // 6. Change the value of the member below to be the sum of numOne, numTwo and numThree divided by numOne
-        public int numBytes = (numOne + numTwo + numThree) / numOne;
+        public int numBytes = 0;
 
 
         // 7. Create a public static char member named lastLetter containing the last letter of the English alphabet
-        public  char lastLetter = 'Z';
+        public char lastLetter = ' ';
 
 
         // 8. Create a public static float member named pi that contains the value of pi to two decimal places
-        public float pi = 3.14f;
+        public float pi = 0f;
 
 
         // 9. Create a public static double member named piD that contains the value of pi to 5 decimal places
-        public  double piD = 3.14159d;
+        public double piD = 0;
 
 
         public static string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -52,25 +52,25 @@ namespace csharp_fundamentals_primitive_types.Main
         public static string lastName = "Smith";
 
         // 10. Create a public static member named fullName that contains the value of firstName and lastName concatenated together with a space in between
-        public  string fullName = $"{firstName} {lastName}";
+        public string fullName = "";
 
 
         // 11. Fix the line below so that tenthLetter contains the tenth letter in the alphabet member above.
-        public char tenthLetter = alphabet.ToCharArray().ElementAtOrDefault(9);
+        public char tenthLetter = alphabet.ToCharArray().ElementAtOrDefault(0);
 
 
         // 12. Create a public static string member named lowerAlphabet that contains the value of the alphabet member in all lower case characters
         // If you need help, look through the available String methods to find a relevant one here: https://www.w3schools.com/cs/cs_strings.php
-        public string lowerAlphabet = alphabet.ToLower();
+        public string lowerAlphabet = "";
 
 
         // 13. Create a public static integer member named alphabetLength that contains the number of characters that exist in the alphabet member
         // Use the documentation linked above if you need help
-        public int alphabetLength = alphabet.Length;
+        public int alphabetLength = 0;
 
 
         // 14. Create a public static integer member named remainder that contains the remainder of dividing 15 by 8
-        public int remainder = 15 % 8;
+        public int remainder = 0;
     }
 
 }
